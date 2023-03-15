270da8323dc6d70dbdacdec003077e1ca490f117
B6Infinity <broteendas.official@gmail.com>
2023-03-15 21:29:15 +0530

    lookin good

--- lib/pages/transactionscreen.dart
+++ lib/pages/transactionscreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'dart:math' as math;
 
 class TransactionScreen extends StatefulWidget {
   const TransactionScreen({super.key});
@@ -12,10 +13,193 @@ class TransactionScreen extends StatefulWidget {
 class _TransactionScreenState extends State<TransactionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    double wid = 110;
+    return Center(
       child: Scaffold(
         body: SafeArea(
-          child: Text('ello moppet'),
+          child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const ListTile(
+                leading: Icon(Icons.people),
+                title: Text(
+                  'moppet',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                subtitle: Text(
+                  'Welcome',
+                  style: TextStyle(fontSize: 12, color: Colors.grey),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Center(
+                child: Text(
+                  'Your Rucksack',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 124, 124, 124),
+                  ),
+                ),
+              ),
+              const Center(
+                child: Padding(
+                  padding: EdgeInsets.all(18),
+                  child: Text(
+                    '₹ 69,420 /-',
+                    style: TextStyle(
+                      fontSize: 32,
+                      letterSpacing: 2,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    height: 150,
+                    width: wid,
+                    child: DecoratedBox(
+                      decoration: const BoxDecoration(
+                        gradient: SweepGradient(
+                          colors: [
+                            Colors.blue,
+                            Colors.white,
+                          ],
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            const Text(
+                              'Transfer',
+                              textDirection: TextDirection.rtl,
+                              style: TextStyle(
+                                color: Colors.blueGrey,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Transform.rotate(
+                              angle: -45 * math.pi / 180,
+                              child: const Icon(
+                                Icons.send,
+                                color: Colors.white,
+                                shadows: [
+                                  Shadow(
+                                    color: Colors.black,
+                                    offset: Offset(2, 2),
+                                    blurRadius: 10,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 150,
+                    width: wid,
+                    child: DecoratedBox(
+                      decoration: const BoxDecoration(
+                        gradient: SweepGradient(
+                          colors: [
+                            Colors.blueGrey,
+                            Colors.white,
+                          ],
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            const Text(
+                              'Request',
+                              textDirection: TextDirection.rtl,
+                              style: TextStyle(
+                                color: Colors.blueGrey,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Transform.rotate(
+                              angle: -235 * math.pi / 180,
+                              child: const Icon(
+                                Icons.send,
+                                color: Colors.white,
+                                shadows: [
+                                  Shadow(
+                                    color: Colors.black,
+                                    offset: Offset(2, 2),
+                                    blurRadius: 10,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 150,
+                    width: wid,
+                    child: DecoratedBox(
+                      decoration: const BoxDecoration(
+                        gradient: SweepGradient(
+                          colors: [
+                            Colors.grey,
+                            Colors.white,
+                          ],
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: const [
+                            Text(
+                              'Top-Up',
+                              textDirection: TextDirection.rtl,
+                              style: TextStyle(
+                                color: Colors.blueGrey,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Icon(
+                              Icons.add_box,
+                              color: Colors.white,
+                              shadows: [
+                                Shadow(
+                                  color: Colors.black,
+                                  offset: Offset(2, 2),
+                                  blurRadius: 10,
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
